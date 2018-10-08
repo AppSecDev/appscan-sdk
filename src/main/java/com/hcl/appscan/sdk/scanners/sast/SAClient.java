f045544b65fceabccc05b6600e51ddc8accdaf55
anurag <anurag-s@hcl.com>
2018-10-08 12:20:30 +0530

    to add the argument for open sournce project

--- src/main/java/com/hcl/appscan/sdk/scanners/sast/SAClient.java
+++ src/main/java/com/hcl/appscan/sdk/scanners/sast/SAClient.java
@@ -269,6 +269,8 @@ public class SAClient implements SASTConstants {
 			args.add(OPT_VERBOSE);
 		if(properties.containsKey(THIRD_PARTY))
 			args.add(OPT_THIRD_PARTY);
+                if (properties.containsKey(OPEN_SOURCE_ONLY) && Boolean.getBoolean(properties.get(OPEN_SOURCE_ONLY))==true)
+                        args.add(OPT_OPEN_SOURCE_ONLY);
 		
 		return args;
 	}
