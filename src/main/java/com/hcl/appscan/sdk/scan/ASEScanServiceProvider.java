a7f0f5330360005ca53d6d92dcabcdb60108dffd
pooja-singh-appscan <62934151+pooja-singh-appscan@users.noreply.github.com>
2020-04-15 17:26:39 +0530

    ASA-2143 (#38)
    * ASA-2143
    
    * Code alignment
    
    Co-authored-by: pooja-singh-appscan <pooja-singh-appscan@users.noreply.github.com>

--- src/main/java/com/hcl/appscan/sdk/scan/ASEScanServiceProvider.java
+++ src/main/java/com/hcl/appscan/sdk/scan/ASEScanServiceProvider.java
@@ -145,7 +145,14 @@ public class ASEScanServiceProvider implements IScanServiceProvider, Serializabl
 		if(!params.get("scanType").isEmpty() && !scanTypeJob(params, jobId)) {
 		    return null;
 		}
-		
+
+		// Test Optimization
+		if(!params.get("testOptimization").isEmpty() &&
+		        !updatescantJob(getUpdatescantJobParams("TestOptimization",
+				params.get("testOptimization"), "false"), jobId)) {
+		   return null;
+		}
+
 		return jobId;
 	}
     
